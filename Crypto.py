65a6ff632df0facb8c259d91856d6566671401cc
Pawel Boening <pawel.boening@web.de>
2023-10-24 18:41:52 +0200

    Formatting
    Formatting

--- Crypto.py
+++ Crypto.py
@@ -7,19 +7,18 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
+
 def smallSign(data, key):
-    result = {
-        'data': data,
-        'small_signature': ""
-    }
+    result = {"data": data, "small_signature": ""}
 
     hashFunction = hashlib.sha1()
     hashFunction.update(data.encode())
     hashFunction.update(key.encode())
     signature = base64.b64encode(hashFunction.digest()).decode()
 
-    result['small_signature'] = signature
+    result["small_signature"] = signature
     return result
+
 
 def smallVerify(data, key):
     oldSignature = data["small_signature"]
@@ -32,6 +31,7 @@ def smallVerify(data, key):
 
     return signature == oldSignature
 
+
 def json_to_qrcode(data):
     qr = qrcode.QRCode(
         version=1,
@@ -40,28 +40,25 @@ def json_to_qrcode(data):
     qr.add_data(data)
     return qr.make_image(fill_color="black", back_color="white")
 
+
 def sign(private_key, private_key_pass, data):
-    result = {
-        'data': data,
-        'b64_signature': ""
-    }
+    result = {"data": data, "b64_signature": ""}
 
     private_key = serialization.load_pem_private_key(
-        private_key.encode(),
-        password=private_key_pass.encode()
+        private_key.encode(), password=private_key_pass.encode()
     )
 
-    signature = private_key.sign( # type: ignore
+    signature = private_key.sign(  # type: ignore
         data.encode(),
         padding.PSS(
-            mgf=padding.MGF1(hashes.SHA1()),
-            salt_length=padding.PSS.DIGEST_LENGTH
-        ), # type: ignore
-        hashes.SHA1() # type: ignore
+            mgf=padding.MGF1(hashes.SHA1()), salt_length=padding.PSS.DIGEST_LENGTH
+        ),  # type: ignore
+        hashes.SHA1(),  # type: ignore
     )
 
-    result['b64_signature'] = base64.b64encode(signature).decode()
+    result["b64_signature"] = base64.b64encode(signature).decode()
     return result
+
 
 def verify(signed_data, public_key):
     data = signed_data["data"]
@@ -75,30 +72,27 @@ def verify(signed_data, public_key):
             signature,
             data.encode(),
             padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
+                mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
             ),
-            hashes.SHA256()
+            hashes.SHA256(),
         )
         return True
-    except:
+    except Exception as e:
         return False
 
+
 def generate_new_keypair(password):
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048
-    )
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 
     encrypted_pem_private_key = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.BestAvailableEncryption(password.encode())
+        encryption_algorithm=serialization.BestAvailableEncryption(password.encode()),
     )
 
     pem_public_key = private_key.public_key().public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
     )
 
     return pem_public_key.decode(), encrypted_pem_private_key.decode()
